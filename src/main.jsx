449fa638fd2564396900349b6fc8aecd52f0112d
CodeAneeq <codewithaneeq.com>
2024-07-06 03:19:59 +0500

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import {
+  BrowserRouter, Routes, Route
+} from 'react-router-dom';
+import { AppProvider } from './context/context';
+import './index.css';
+import Home from './pages/home/home';
+import About from "./pages/about/about";
+import BookList from "./components/bookList/bookList";
+import BookDetails from "./components/bookdetails/bookdetails";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <AppProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<Home />}>
+          <Route path = "about" element = {<About />} />
+          <Route path = "book" element = {<BookList />} />
+          <Route path = "/book/:id" element = {<BookDetails />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </AppProvider>
+);
